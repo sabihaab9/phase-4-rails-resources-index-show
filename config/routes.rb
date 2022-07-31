8e2409dcfc2ca0c46d05daeb3072c98b58a2aa81
Sabiha Bary <sabihaab9@gmail.com>
2022-07-31 14:26:11 -0400

    commit tree 9b67e4f9c1bded913c4493324693fe20e3140dfc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+#To set up our index and show actions, 
+#we'll first need to create some routes. Recall:
+
+  # GET /birds      => show a list of all birds
+  # GET /birds/:id  => show one specific bird
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+  resources :birds, only: [:index, :show]
 end
